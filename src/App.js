12f11a9a3dc0f2a51ce92fddaaeae258be9a7237
kent auyeung <kentayeung@gmail.com>
2021-06-08 16:46:13 -0700

    Refactored Display and Button Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { useState } from 'react'
 
-const Display = ({ counter }) => {
-  return (
-    <div>{counter}</div>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
 
-const Button = (props) => {
-  return (
-    <button onClick={props.handleClick}>
-      {props.text}
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
     </button>
   )
-}
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
