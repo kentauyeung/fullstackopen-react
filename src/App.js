cc0647b56a8ebfdb808b1bd5e4f5b90ed5983201
kent auyeung <kentayeung@gmail.com>
2021-06-08 14:55:39 -0700

    Completed exercises 1.3 - 1.5

--- src/App.js
+++ src/App.js
@@ -3,46 +3,29 @@ import React from 'react'
 
 
 const App = () => {
-
-  const Header = () => {
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercise = 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercise = 7
+      },
+      {
+        name: 'Start of a component',
+        exercise = 14
+      }
+    ]
   }
-  
-  const Content = () => {
-    return (
-      <div>
-        <Part />
-        <Part />
-        <Part />
-      </div>
-    )
-  }
-  
-  const Total = () => {
-  
-  }
-
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercise1 = 10
-  const part2 = 'Using props to pass data'
-  const exercise2 = 7
-  const part3 = 'Start of a component'
-  const exercise3 = 14
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercise1}
-      </p>
-      <p>
-        {part2} {exercise2}
-      </p>
-      <p>
-        {part3} {exercise3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+     <Header course = {course} />
+     <Content course = {parts} />
+     <Total parts = {parts} />
     </div>
   )
 }
