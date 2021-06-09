e215ffeb9105973f421ee6b9dc7123caff95c875
kent auyeung <kentayeung@gmail.com>
2021-06-09 13:01:00 -0700

    Completed exercise 1.7

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ const Button = (props) => (
   </button>
 )
 
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [bad, setBad] = useState(0)
@@ -35,10 +36,15 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <h3>Stats</h3>
-      <div>Good {good}</div>
-      <div>Bad {bad}</div>
-      <div>Neutral {neutral}</div>
+      <div>
+        <h3>Stats</h3>
+        <div>Good {good}</div>
+        <div>Bad {bad}</div>
+        <div>Neutral {neutral}</div>
+        <div>All {good + bad + neutral}</div>
+        <div>Average { (good + bad + neutral) / good - bad }</div>
+        <div>Positive {(good / (good + bad + neutral)) * 100}%</div>
+      </div>
     </div>
   )
 }
